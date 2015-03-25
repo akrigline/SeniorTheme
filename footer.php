dd720e78784c316c483ecfe1e19b37bf4e0ec9c4
akrigline <andrew@akrigline.com>
2015-03-25 19:34:49 -0400

    minified and gutted js

--- footer.php
+++ footer.php
@@ -10,9 +10,9 @@
 
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
   <script>window.jQuery || document.write('<script src="<?php bloginfo('template_directory');?>/js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
-  <script src="<?php bloginfo('template_directory');?>/js/vendor/jquery.fullPage.js"></script>
+  <script src="<?php bloginfo('template_directory');?>/js/vendor/jquery.fullPage.min.js"></script>
   <script src="<?php bloginfo('template_directory');?>/js/vendor/imagesloaded.pkgd.min.js"></script>
-  <script src="<?php bloginfo('template_directory');?>/js/vendor/bootstrap.js"></script>
+  <script src="<?php bloginfo('template_directory');?>/js/vendor/bootstrap.min.js"></script>
 
   <script src="<?php bloginfo('template_directory');?>/js/main.js"></script>
 
